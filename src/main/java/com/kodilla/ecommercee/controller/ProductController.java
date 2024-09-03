ee7e4d9ed1c795bc13958d11f8e5e580a992b678
Krzysztof Fit <krzysztof.fit@hotmail.com>
2024-09-03 22:14:45 +0200

    JDP240901-1 Added Swagger annotations

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,9 +1,10 @@
 package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.domain.ProductDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -11,18 +12,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v1/products")
+@Tag(name = "Products", description = "Managing products")
 public class ProductController {
 
+    @Operation(
+            description = "Get products list from database",
+            summary = "Get products"
+    )
     @GetMapping
     public List<ProductDto> getProducts() {
         return new ArrayList<>();
     }
 
+    @Operation(
+            description = "Get product from database",
+            summary = "Get product"
+    )
     @GetMapping(value = "{productId}")
     public ProductDto getProduct(@PathVariable Long productId) {
         return new ProductDto(
                 1L,
                 "Bluetooth speaker",
+                "Some nice speaker",
                 BigDecimal.valueOf(249),
                 50,
                 1,
@@ -30,16 +41,25 @@ public class ProductController {
                 );
     }
 
+    @Operation(
+            description = "Create product in database",
+            summary = "Create product"
+    )
     @PostMapping
     public void createProduct(@RequestBody ProductDto productDto) {
 
     }
 
+    @Operation(
+            description = "Update product in database",
+            summary = "Update product"
+    )
     @PutMapping
     public ProductDto updateProduct(@RequestBody ProductDto productDto) {
         return new ProductDto(
                 1L,
                 "Bluetooth speaker plus",
+                "Speaker with extra function",
                 BigDecimal.valueOf(289),
                 50,
                 1,
@@ -47,6 +67,10 @@ public class ProductController {
         );
     }
 
+    @Operation(
+            description = "Delete product from database",
+            summary = "Delete product"
+    )
     @DeleteMapping(value = "{productId}")
     public void deleteProduct(@PathVariable Long productId) {
 
--- src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -1,5 +1,6 @@
 package com.kodilla.ecommercee.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -8,11 +9,18 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class ProductDto {
 
+    @Schema(description = "Unique identifier of the product", example = "1")
     private final Long productId;
+    @Schema(description = "Product name of the product", example = "Smartphone")
     private final String productName;
-    private String description;
+    @Schema(description = "Description of the product", example = "6-inch")
+    private final String description;
+    @Schema(description = "Product price", example = "299.99")
     private final BigDecimal price;
+    @Schema(description = "Quantity of the product", example = "100")
     private final int quantity;
+    @Schema(description = "Id group for product", example = "1")
     private final int productGroupId;
+    @Schema(description = "Product creation date", example = "2024-09-03T20:30:45")
     private final LocalDate createDate;
 }
