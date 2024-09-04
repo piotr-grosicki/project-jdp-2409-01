1c021a68e859bbb4c52d521d38849b30d49f3bb8
Krzysztof Fit <krzysztof.fit@hotmail.com>
2024-09-04 21:19:22 +0200

    JDP240901-26 & JDP240901-27 Methods updated

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,6 +1,11 @@
 package com.kodilla.ecommercee.controller;
 
+import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import com.kodilla.ecommercee.domain.ProductDto;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
@@ -13,43 +18,53 @@ import java.util.List;
 @RequestMapping("v1/products")
 public class ProductController {
 
-    @GetMapping
-    public List<ProductDto> getProducts() {
-        return new ArrayList<>();
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<ProductDto>> getProducts() {
+        return ResponseEntity.ok(
+                List.of(new ProductDto(
+                        2L,
+                        "Socks",
+                        "Big white socks",
+                        BigDecimal.valueOf(2.99),
+                        500,
+                        18,
+                        LocalDate.of(2024, 9, 6)
+                ))
+        );
     }
 
-    @GetMapping(value = "{productId}")
-    public ProductDto getProduct(@PathVariable Long productId) {
-        return new ProductDto(
+    @GetMapping(value = "/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long productId) {
+        return ResponseEntity.ok(new ProductDto(
                 1L,
                 "Bluetooth speaker",
+                "Great speaker",
                 BigDecimal.valueOf(249),
                 50,
                 1,
                 LocalDate.of(2024, 8, 31)
-                );
-    }
-
-    @PostMapping
-    public void createProduct(@RequestBody ProductDto productDto) {
-
-    }
-
-    @PutMapping
-    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
-        return new ProductDto(
-                1L,
-                "Bluetooth speaker plus",
-                BigDecimal.valueOf(289),
-                50,
-                1,
-                LocalDate.of(2024, 8,31)
+                )
         );
     }
 
-    @DeleteMapping(value = "{productId}")
-    public void deleteProduct(@PathVariable Long productId) {
+    @PostMapping(value = "/{productId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Void> createProduct(@RequestBody ProductDto productDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
+    @PutMapping(value = "/{productId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Void> updateProduct(@PathVariable Long productId, @RequestBody ProductDto productDto) {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping(value = "/{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
--- src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -5,12 +5,13 @@ import lombok.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
 @RequiredArgsConstructor
 public class ProductDto {
 
     private final Long productId;
     private final String productName;
-    private String description;
+    private final String description;
     private final BigDecimal price;
     private final int quantity;
     private final int productGroupId;
