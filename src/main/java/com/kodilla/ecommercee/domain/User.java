7dd127828decb4026999ae521ab554029fd13cc3
Bogdan Brandys <brandpol@poczta.onet.pl>
2024-09-05 16:50:09 +0200

    Added User entity

---
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -0,0 +1,43 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "USER_ID")
+    private Long userId;
+    @NotNull
+    @Column(name = "USERNAME", unique = true)
+    private String username;
+    @Column(name = "FIRSTNAME")
+    private String firstName;
+    @Column(name = "LASTNAME")
+    private String lastName;
+    @NotNull
+    @Column(name = "LOGIN(EMAIL)", unique = true)
+    private String email;
+    @NotNull
+    @Column(name = "PASSWORD")
+    private String password;
+    @NotNull
+    @Column(name = "STATUS")
+    private String status;
+    @NotNull
+    @Column(name = "TEMPORARY_KEY")
+    private int temporaryKey;
+    @NotNull
+    @Column(name = "CREATE_DATE")
+    private LocalDate createDate;
+}
