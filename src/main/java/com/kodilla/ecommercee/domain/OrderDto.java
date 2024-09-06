5ee11070b1d57783ee8ac0f5665ccbb536a1fc6f
Roman Khudoley <romankhudoley@gmail.com>
2024-09-06 11:18:28 +0200

    JDP240901-16 Added OrderRepositoryTests & OrderRepository inteface

--- src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -22,4 +23,7 @@ public class OrderDto {
     private LocalDateTime creationDate;
     @Schema(description = "Total amount to be paid for the order", example = "150.00")
     private BigDecimal totalAmount;
+
+    public OrderDto(long id, LocalDate of, BigDecimal bigDecimal, String delivered, long l) {
+    }
 }
