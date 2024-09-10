de8b67555b4f1b955de81ab714fcc4f6ef2e3bcf
Bogdan Brandys <brandpol@poczta.onet.pl>
2024-09-10 20:07:19 +0200

    JDP240901-46 fixes to ProductRepository tests

--- src/test/java/com/kodilla/ecommercee/repository/ProductRepositoryTests.java
+++ src/test/java/com/kodilla/ecommercee/repository/ProductRepositoryTests.java
@@ -29,34 +29,36 @@ public class ProductRepositoryTests {
     @Test
     public void getAll() {
         //Given
-        Product product = new Product(1L, "Toyota", "Car", new BigDecimal(55000), 1, null, LocalDate.now());
+        Product product = new Product(null, "Toyota", "Car", new BigDecimal(55000), 1, null, LocalDate.now());
         Product savedProduct = productRepository.save(product);
-        Product product2 = new Product(2L, "Yamaha", "Motorcycle", new BigDecimal(16000), 1, null, LocalDate.now());
+        Long productId = savedProduct.getId();
+        Product product2 = new Product(null, "Yamaha", "Motorcycle", new BigDecimal(16000), 1, null, LocalDate.now());
         Product savedProduct2 = productRepository.save(product2);
+        Long productId2 = savedProduct2.getId();
         //When
         Iterable<Product> products = productRepository.findAll();
         List<Product> listOfProducts = StreamSupport.stream(products.spliterator(), false).collect(Collectors.toList());
         //Then
         Assertions.assertEquals(2, listOfProducts.size());
-        Assertions.assertEquals("Yamaha", listOfProducts.get(1).getName());
         //Clean Up
-        productRepository.deleteById(product.getId());
-        productRepository.deleteById(product2.getId());
+        productRepository.deleteById(productId);
+        productRepository.deleteById(productId2);
     }
 
     @DisplayName("Test case for save method")
     @Test
     public void shouldSaveProduct() {
         //Given
-        Product product = new Product(1L, "Toyota", "Car", new BigDecimal(55000), 1, null, LocalDate.now());
+        Product product = new Product(null, "Toyota", "Car", new BigDecimal(55000), 1, null, LocalDate.now());
         Product savedProduct = productRepository.save(product);
+        Long productId = savedProduct.getId();
         //When
-        Optional<Product> productSaved = productRepository.findById(savedProduct.getId());
+        Optional<Product> productSaved = productRepository.findById(productId);
         //Then
         Assertions.assertTrue(productSaved.isPresent());
         Assertions.assertEquals("Toyota", productSaved.get().getName());
         //Clean Up
-        productRepository.delete(product);
+        productRepository.deleteById(productId);
     }
     @DisplayName("Test case for delete method")
     @Test
@@ -64,18 +66,19 @@ public class ProductRepositoryTests {
         //Given
         Group group = new Group(null,"Small SUV", null, null, null, LocalDateTime.now());
         Group savedGroup = groupRepository.save(group);
-        Product product = new Product(1L, "Toyota", "Car", new BigDecimal(55000), 1, group, LocalDate.now());
+        Long groupId = savedGroup.getId();
+        Product product = new Product(null, "Toyota", "Car", new BigDecimal(55000), 1, group, LocalDate.now());
         Product savedProduct = productRepository.save(product);
+        Long productId = savedProduct.getId();
         //When
-        Optional<Group> groupSaved = groupRepository.findById(savedGroup.getId());
-        productRepository.delete(savedProduct);
+        Optional<Group> groupSaved = groupRepository.findById(groupId);
+        productRepository.deleteById(productId);
         //Then
-        Assertions.assertFalse(productRepository.findById(savedProduct.getId()).isPresent());
+        Assertions.assertFalse(productRepository.findById(productId).isPresent());
         Assertions.assertTrue(groupSaved.isPresent());
         Assertions.assertEquals("Small SUV", groupSaved.get().getName());
         //Clean up
-        productRepository.delete(product);
-        groupRepository.delete(group);
+        groupRepository.deleteById(groupId);
     }
 
 }
