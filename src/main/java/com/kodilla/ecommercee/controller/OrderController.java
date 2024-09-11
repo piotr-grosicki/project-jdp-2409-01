4070793da06f15e9e2927ff49c483c09a88d7adc
Krzysztof Fit <krzysztof.fit@hotmail.com>
2024-09-11 21:19:51 +0200

    JDP240901-47 Missing endpoints added

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/orders")
@@ -18,6 +20,34 @@ public class OrderController {
 
     private final OrderDbService orderDbService;
 
+    @Operation(
+            description = "Retrieve all orders",
+            summary = "Get all orders"
+    )
+    @GetMapping
+    public ResponseEntity<List<OrderDto>> getOrders() {
+        List<OrderDto> orders = orderDbService.getAllOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @Operation(
+            description = "Retrieve a specific order",
+            summary = "Get order by id"
+    )
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDto> getOrder(@PathVariable Long orderId) {
+        try {
+            OrderDto orderDto = orderDbService.getOrderById(orderId);
+            return ResponseEntity.ok(orderDto);
+        } catch (OrderNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @Operation(
+            description = "Create a new order",
+            summary = "Create order"
+    )
     @PostMapping
     public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto orderDto) throws UserNotFoundException, CartNotFoundException {
         OrderDto createdOrder = orderDbService.createOrder(orderDto);
@@ -32,5 +62,19 @@ public class OrderController {
     public ResponseEntity<OrderDto> updateOrder(@PathVariable Long orderId, @RequestBody OrderDto orderDto) throws OrderNotFoundException, UserNotFoundException, CartNotFoundException {
         OrderDto updatedOrder = orderDbService.updateOrder(orderId, orderDto);
         return ResponseEntity.ok(updatedOrder);
+    }
+
+    @Operation(
+            description = "Delete order by id",
+            summary = "Delete order"
+    )
+    @DeleteMapping("/{orderdId}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderdId) {
+        boolean isDeleted = orderDbService.deleteOrder(orderdId);
+        if (isDeleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
