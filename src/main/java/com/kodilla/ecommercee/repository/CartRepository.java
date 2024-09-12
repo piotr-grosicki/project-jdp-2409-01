6bdf10ce774f3448850927c2f5cb6261969fdabc
Marta Kowalczyk <martusia@mat.umk.pl>
2024-09-12 17:12:31 +0200

    Task JDP240901-40: ProductRepositoryTests added.

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -1,9 +1,11 @@
 package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,5 @@ public interface CartRepository extends CrudRepository<Cart, Long> {
     Cart save(Cart cart);
     @Override
     Optional<Cart> findById(Long id);
+    List<Cart> findByCartProductsContains(Product product);
 }
